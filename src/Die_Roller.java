26916ef0274f1a629953cd8a7b492e8384afd8bd
adam <adam@gomaafam.com>
2024-10-10 13:42:05 -0400

    Die Roller

--- src/Die_Roller.java
+++ src/Die_Roller.java
@@ -1,15 +1,29 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class Die_Roller {
     public static void main(String[] args) {
+        boolean done = false;
+        Scanner in = new Scanner(System.in);
         Random gen = new Random();
-        int die1, die2, die3 = 0;
+        int die1, die2, die3, sum = 0;
         int dieRoll = 0;
+
         do {
-            die1 = gen.nextInt(6) + 1;
-            die2 = gen.nextInt(6) + 1;
-            die3 = gen.nextInt(6) + 1;
-            dieRoll++;
-            System.out.printf("%6d\t%4d\t%4d\t%4d\n", dieRoll, die1, die2, die3);
-        } while (!(die1 == die2 && die2 == die3));
+               dieRoll = 0;
+            do {
+                die1 = gen.nextInt(6) + 1;
+                die2 = gen.nextInt(6) + 1;
+                die3 = gen.nextInt(6) + 1;
+                sum = die1 + die2 + die3;
+                dieRoll++;
+                System.out.printf("%6d\t%4d\t%4d\t%4d\t%6d\n", dieRoll, die1, die2, die3,sum);
+            } while (!(die1 == die2 && die2 == die3));
+
+            System.out.print("Play again? [Y/N]: ");
+            String continueYN = in.nextLine();
+            if (continueYN.equalsIgnoreCase("n"))
+                done = true;
+        }while (!done);
     }
 }
