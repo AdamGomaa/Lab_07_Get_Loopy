c67f8cdc68d08fed70ea7e95cef466f8a46d32bd
adam <adam@gomaafam.com>
2024-10-10 11:44:12 -0400

    Loop 04

---
+++ src/Loop_04.java
@@ -0,0 +1,7 @@
+public class Loop_04 {
+    public static void main(String[] args){
+        System.out.println();
+        for (int c = 10; c >= 0; c -=2)
+            System.out.print("\t" + c);
+    }
+}
