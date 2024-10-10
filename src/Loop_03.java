8204b49e93b7a45484c83b00e08985b19350a0d1
adam <adam@gomaafam.com>
2024-10-10 11:41:49 -0400

    Loop 03

---
+++ src/Loop_03.java
@@ -0,0 +1,7 @@
+public class Loop_03 {
+    public static void main(String[] args){
+        System.out.println();
+        for (int c = 0; c <= 18; c+= 3)
+            System.out.print("\t" + c);
+    }
+}
