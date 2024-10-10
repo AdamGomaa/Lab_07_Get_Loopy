8873bc6682a9c8f1b57225fee38e6e37e897f333
adam <adam@gomaafam.com>
2024-10-10 11:22:23 -0400

    Loop 02

---
+++ src/Loop_02.java
@@ -0,0 +1,7 @@
+public class Loop_02 {
+    public static void main(String[] args){
+        System.out.println();
+        for (int c = 30; c >= 0; c--)
+            System.out.print("\t" + c);
+    }
+}
